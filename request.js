619057a50f7c1dae652948cd152f39794431af77
Darcy McLean <darc.mclean@gmail.com>
2017-10-09 18:13:35 +0000

    LHL w2d1 http request update

---
+++ request.js
@@ -0,0 +1,29 @@
+/* 20171009 DM - LHL w2d1
+
+  http requests exercise
+
+*/
+
+// require `request` and the Node `fs` (filesystem) module
+var request = require('request');
+var fs = require('fs');
+
+// Using Try/Catch to handle errors.
+try {
+
+request.get('https://sytantris.github.io/http-examples/future.jpg')               // Note 1
+       .on('error', function (err) {
+        throw err;                                // Note 2
+       })
+
+       .on('response', function (response) {                           // Note 3
+         console.log('Response Status Code: ', response.statusCode);
+         console.log('Response Message: ', response.statusMessage);
+         console.log('Response Content Type: ', response.headers['content-type']);
+       })
+
+       .pipe(fs.createWriteStream('./future.jpg'));
+
+} catch (e) {
+  console.log("Error downloading file - ", e.message);
+}
